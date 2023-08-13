3dbd2a40566a13654149e22beb2259b359bf8c1e
Yaroslav <yaroslav.kramarchuk@gmail.com>
2023-08-13 19:14:53 +0200

    breakpoints up to 1440, all styles and positioning

--- index.js
+++ index.js
@@ -13,3 +13,26 @@
 // 		});
 // 	});
 // });
+
+// document.addEventListener("DOMContentLoaded", function () {
+// 	const faqTextElements = document.querySelectorAll(".faq__text");
+
+// 	faqTextElements.forEach(function (element) {
+// 		element.addEventListener("click", function () {
+// 			const description = this.nextElementSibling;
+// 			description.classList.toggle("hidden");
+// 		});
+// 	});
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+	const faqTextElements = document.querySelectorAll(".faq__text");
+
+	faqTextElements.forEach(function (element) {
+		element.addEventListener("click", function () {
+			const faqContent = this.closest(".faq__content");
+			const description = faqContent.querySelector(".faq__description");
+			description.classList.toggle("hidden");
+		});
+	});
+});

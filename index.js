14061bdc5d8d796be2765afcc71dc0a2e92dae34
Yaroslav <yaroslav.kramarchuk@gmail.com>
2023-08-13 22:32:10 +0200

    path to img, fixed

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-// 	const button = document.querySelector(".channels__button");
-// 	const hiddenItems = document.querySelectorAll(
-// 		".channels__item:nth-child(n+7)"
-// 	);
-
-// 	button.addEventListener("click", function () {
-// 		hiddenItems.forEach((item) => {
-// 			item.style.display =
-// 				item.style.display === "none" || item.style.display === ""
-// 					? "block"
-// 					: "none";
-// 		});
-// 	});
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-// 	const faqTextElements = document.querySelectorAll(".faq__text");
-
-// 	faqTextElements.forEach(function (element) {
-// 		element.addEventListener("click", function () {
-// 			const description = this.nextElementSibling;
-// 			description.classList.toggle("hidden");
-// 		});
-// 	});
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
 	const faqTextElements = document.querySelectorAll(".faq__text");
 

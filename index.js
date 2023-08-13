ad1bef16e260b4df85d1ed5f9af6b28bcbea16f3
Yaroslav <yaroslav.kramarchuk@gmail.com>
2023-08-13 17:06:26 +0200

    breakpoints up to 990, advantages list added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-	const button = document.querySelector(".channels__button");
-	const hiddenItems = document.querySelectorAll(
-		".channels__item:nth-child(n+7)"
-	);
+// document.addEventListener("DOMContentLoaded", function () {
+// 	const button = document.querySelector(".channels__button");
+// 	const hiddenItems = document.querySelectorAll(
+// 		".channels__item:nth-child(n+7)"
+// 	);
 
-	button.addEventListener("click", function () {
-		hiddenItems.forEach((item) => {
-			item.style.display =
-				item.style.display === "none" || item.style.display === ""
-					? "block"
-					: "none";
-		});
-	});
-});
+// 	button.addEventListener("click", function () {
+// 		hiddenItems.forEach((item) => {
+// 			item.style.display =
+// 				item.style.display === "none" || item.style.display === ""
+// 					? "block"
+// 					: "none";
+// 		});
+// 	});
+// });

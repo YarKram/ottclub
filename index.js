f8245b6242ba0edbef65f0ac778e27c70ec99de4
Yaroslav Kramarchuk <yaroslav.kramarchuk@gmail.com>
2023-08-28 13:19:03 +0200

    vertical scroll added, header img positioned

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
-	const faqTextElements = document.querySelectorAll(".faq__text");
+const faqItems = document.querySelectorAll(".faq__item");
 
-	faqTextElements.forEach(function (element) {
-		element.addEventListener("click", function () {
-			const faqContent = this.closest(".faq__content");
-			const description = faqContent.querySelector(".faq__description");
-			description.classList.toggle("hidden");
-		});
+faqItems.forEach((faqItem) => {
+	faqItem.addEventListener("click", () => {
+		const faqDescription = faqItem.querySelector(".faq__description");
+		faqDescription.classList.toggle("hidden");
 	});
+});
+
+// chanells list scroilling
+const slider = document.querySelector(".catalog-list");
+const preventClick = (e) => {
+	e.preventDefault();
+	e.stopImmediatePropagation();
+};
+let isDown = false;
+let isDragged = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+	isDown = true;
+	slider.classList.add("active");
+	startX = e.pageX - slider.offsetLeft;
+	scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+	isDown = false;
+	slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", (e) => {
+	isDown = false;
+	const elements = document.querySelectorAll("a");
+	if (isDragged) {
+		for (let i = 0; i < elements.length; i++) {
+			elements[i].addEventListener("click", preventClick);
+		}
+	} else {
+		for (let i = 0; i < elements.length; i++) {
+			elements[i].removeEventListener("click", preventClick);
+		}
+	}
+	slider.classList.remove("active");
+	isDragged = false;
+});
+slider.addEventListener("mousemove", (e) => {
+	if (!isDown) return;
+	isDragged = true;
+	e.preventDefault();
+	const x = e.pageX - slider.offsetLeft;
+	const walk = (x - startX) * 2;
+	slider.scrollLeft = scrollLeft - walk;
+	console.log(walk);
 });
